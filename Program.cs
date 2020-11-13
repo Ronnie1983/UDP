86972aba4ab134e879236ee3f4e1d93b767d6e20
Ronnie Damsgaard <Ronnie1983@me.com>
2020-11-13 10:51:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.start();
+        }
+    }
+}
